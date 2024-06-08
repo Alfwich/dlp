7c4c17f9f3cee430e258b260f0cdb6b61617f92f
aw <aw@aw.com>
2024-06-07 21:20:06 -0700

    Updates

--- download.py
+++ download.py
@@ -8,6 +8,8 @@ import json
 import urllib
 import shutil
 import string
+import time
+import stat
 
 from pathlib import Path
 
@@ -22,18 +24,19 @@ def fs_format(name):
     return "".join(c for c in name if (c.isalpha() or c.isdigit() or c==' ') and c in string.printable).rstrip().replace("  ", " ").replace(" ", "-")
 
 def prepare_dir():
-    # Ensure we are starting from a clean build dir
-    if os.path.exists(build_dir):
-        cleanup_dir()
+    if not os.path.exists(build_dir):
+        os.mkdir(build_dir)
 
-    os.mkdir(build_dir)
 
 def cleanup_dir():
-    shutil.rmtree(build_dir)
+    files_to_prune = list(filter(lambda x: x.name != 'yt-dlp', Path(f"{server_dir}/{build_dir}").iterdir()))
+    for f in files_to_prune:
+        f.unlink()
 
-def find_processed_file(final_type):
+
+def find_processed_file():
     for f in os.listdir("."):
-        if f.endswith(final_type):
+        if f != 'yt-dlp':
             return f
 
 def get_name_from_file(f):
@@ -43,7 +46,7 @@ def move_files(final_type):
     for f in os.listdir(build_dir):
         if f.endswith(final_type):
             log(f"Installing {f}\n") 
-            subprocess.run(["cp", f"{build_dir}/{f}", f"{server_dir}/{f}"])
+            subprocess.run(["cp", f"{build_dir}/{f}", f"{server_dir}/content/{f}"])
 
 def prune_files():
     files_to_prune = list(filter(lambda x: x.suffix == ".mp3" or x.suffix == ".mp4", reversed(sorted(Path(server_dir).iterdir(), key=os.path.getmtime))))[20:]
@@ -66,18 +69,31 @@ def download_video(url, final_type):
     log(f"Downloading video: [{url}] as {final_type} ... \n")
     exec_cmd(["./yt-dlp", url, "--no-playlist"])
     log("Done!\n")
-    video_file = find_processed_file(final_type)
+    video_file = find_processed_file()
     title = f"{get_name_from_file(video_file)}.{final_type}"
-    log(f"Converting video to .{final_type} ... \n")
-    video_options = ["--codec", "copy"] if final_type is "mp4" else []
-    exec_cmd([f"/usr/bin/ffmpeg", "-i", video_file] + video_options + [title])
-    log("Done!\n")
+    existing_file = Path(f"{server_dir}/content/{title}")
+    if not existing_file.exists():
+        log(f"Converting video to .{final_type} ... \n")
+        video_options = ["--codec", "copy"] if final_type is "mp4" else []
+        exec_cmd([f"/usr/bin/ffmpeg", "-i", video_file] + video_options + [title])
+        log("Done!\n")
+    else:
+        log(f"Found existing content file, aborting.\n")
     os.chdir(cwd)
 
+yt_binary_name = "yt-dlp"
 yt_dlp_download_url="https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux_armv7l"
+yt_dlp_ttl = 60 * 60 * 24
 def acquire_yt_dlp():
-    exec_cmd(["wget", yt_dlp_download_url, "-O", "yt-dlp"])
-    exec_cmd(["chmod", "775", "yt-dlp"])
+
+    existing_yt_dlp = Path(yt_binary_name)
+    if existing_yt_dlp.exists():
+        log(f"Found yt-dlp (age={time.time() - os.path.getmtime(yt_binary_name)}s)\n")
+    if not existing_yt_dlp.exists() or time.time() - os.path.getmtime(yt_binary_name) > yt_dlp_ttl:
+        log("Downloading ty-dlp\n")
+        exec_cmd(["wget", yt_dlp_download_url, "-O", "yt-dlp"])
+        exec_cmd(["chmod", "775", "yt-dlp"])
+        exec_cmd(["touch", "yt-dlp"])
 
 def main(video_url, final_type):
     log(f"Executing download url: {video_url}, type: {final_type}\n")
