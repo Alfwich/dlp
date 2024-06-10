19c889f4fe4fd092cc06e4dc728611afe129c344
aw <aw@aw.com>
2024-06-09 18:04:25 -0700

    Update

--- download.py
+++ download.py
@@ -16,7 +16,7 @@ from pathlib import Path
 from datetime import datetime
 
 build_dir = "build"
-server_dir = "/www/wuteri.ch/misc"
+server_dir = "/www/wuteri.ch/dlp"
 
 def log(msg):
     preamble = f"[{datetime.now().strftime('%H:%M:%S:%f')}]"
@@ -26,20 +26,16 @@ def log(msg):
 def fs_format(name):
     return "".join(c for c in name if (c.isalpha() or c.isdigit() or c==' ') and c in string.printable).rstrip().replace("  ", " ").replace(" ", "-")
 
-def prepare_dir():
+def prepare_dir(scope):
     if not os.path.exists(build_dir):
         os.mkdir(build_dir)
 
+    if not os.path.exists(f"{server_dir}/content/{scope}"):
+        os.mkdir(f"{server_dir}/content/{scope}")
+
     exec_cmd(["df", "-h"])
 
-
-def cleanup_dir():
-    files_to_prune = list(filter(lambda x: x.name != 'yt-dlp', Path(f"{server_dir}/{build_dir}").iterdir()))
-    for f in files_to_prune:
-        f.unlink()
-
-
-def find_processed_file():
+def find_downloaded_file():
     for f in os.listdir(f"{server_dir}/{build_dir}"):
         if f != 'yt-dlp':
             return f
@@ -47,14 +43,19 @@ def find_processed_file():
 def get_name_from_file(f):
         return "-".join(fs_format(os.path.splitext(f)[0]).split("-")[0:-1])
 
-def move_files(final_type):
+def move_files(scope, final_type):
     if final_type == "any":
-        final_type = Path(find_processed_file()).suffix.split(".")[-1]
+        final_type = Path(find_downloaded_file()).suffix.split(".")[-1]
 
     for f in os.listdir(build_dir):
         if f.endswith(final_type):
             log(f"Installing {f}\n") 
-            exec_cmd(["cp", f"{build_dir}/{f}", f"{server_dir}/content/{f}"])
+            exec_cmd(["cp", f"{build_dir}/{f}", f"{server_dir}/content/{scope}/{f}"])
+
+def cleanup_dir():
+    files_to_prune = list(filter(lambda x: x.name != 'yt-dlp', Path(f"{server_dir}/{build_dir}").iterdir()))
+    for f in files_to_prune:
+        f.unlink()
 
 def prune_files():
     files_to_prune = list(reversed(sorted(Path(f"{server_dir}/content").iterdir(), key=os.path.getmtime)))[20:]
@@ -70,16 +71,16 @@ def exec_cmd(cmd):
     log(f"Executing command: {pretty_cmd}\n")
     subprocess.run(cmd, stderr=subprocess.STDOUT)
 
-def convert_video(video_file, final_type):
+def convert_video(scope, video_file, final_type):
     title = f"{get_name_from_file(video_file)}.{final_type}"
-    existing_file = Path(f"{server_dir}/content/{title}")
+    existing_file = Path(f"{server_dir}/content/{scope}/{title}")
     if not existing_file.exists():
         log(f"Converting video to .{final_type} ... \n")
         video_options = ["-preset", "ultrafast"] if final_type is "mp4" else []
         exec_cmd([f"/usr/bin/ffmpeg", "-i", video_file] + video_options + [title])
         log("Done!\n")
 
-def download_video(url, final_type):
+def download_and_process_video(scope, url, final_type):
     cwd = os.getcwd()
     os.chdir(build_dir)
     acquire_yt_dlp()
@@ -88,10 +89,10 @@ def download_video(url, final_type):
     log("Done downloading!\n")
 
     if final_type != "any":
-        video_file = find_processed_file()
-        convert_video(video_file, final_type)
+        video_file = find_downloaded_file()
+        convert_video(scope, video_file, final_type)
     else:
-        video_file = find_processed_file()
+        video_file = find_downloaded_file()
         renamed_file = f"{get_name_from_file(video_file)}{Path(video_file).suffix}"
         exec_cmd(["mv", video_file, renamed_file])
 
@@ -118,19 +119,25 @@ def acquire_yt_dlp():
         else: 
             log("Could not acquire yt-dlp binary\n")
 
-def main(video_url, final_type):
-    log(f"Executing download url: {video_url}, type: {final_type}\n")
-    prepare_dir()
-    download_video(video_url, final_type)
-    move_files(final_type)
-    prune_files()
+def process_scope(in_scope):
+    if not in_scope is None and len(in_scope.strip()) > 0:
+        return in_scope.strip()
+
+    return "global"
+
+def main(video_url, final_type, scope):
+    resolved_scope = process_scope(scope)
+    log(f"Executing download url: {video_url}, type: {final_type}, scope: {resolved_scope}\n")
+    prepare_dir(resolved_scope)
+    download_and_process_video(resolved_scope, video_url, final_type)
+    move_files(resolved_scope, final_type)
     cleanup_dir()
     log("Done adding video!\n")
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        log("Expected usage: ./download.py <url> <type>\n")
+        log("Expected usage: ./download.py <url> <type> [scope]\n")
     else:
-        main(sys.argv[1], sys.argv[2])
+        main(sys.argv[1], sys.argv[2], sys.argv[3] if len(sys.argv) > 3 else None)
 
 
--- server-hook.py
+++ server-hook.py
@@ -8,9 +8,10 @@ from download import log
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        log("Expected usage: ./download-server.py <url> <type>\n")
+        log("Expected usage: ./download-server.py <url> <type> [scope]\n")
     else:
         url = base64.b64decode(sys.argv[1]).decode("utf-8")
         desired_type = base64.b64decode(sys.argv[2]).decode("utf-8")
+        scope = base64.b64decode(sys.argv[3]).decode("utf-8") if len(sys.argv) > 3 else ""
 
-        dl_main(url, desired_type)
+        dl_main(url, desired_type, scope)
